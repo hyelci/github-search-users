cf35aaaa1e0c19cc0c954489d7fbb020e90fb8f9
Hacer Yelci <haceryelci@gmail.com>
2022-12-01 11:46:23 +0000

    uptadet user dashboard and added charts

--- src/components/User.js
+++ src/components/User.js
@@ -1,16 +1,16 @@
-import React from 'react';
-import styled from 'styled-components';
-import Card from './Card';
-import Followers from './Followers';
+import React from "react";
+import styled from "styled-components";
+import Card from "./Card";
+import Followers from "./Followers";
 const User = () => {
   return (
-    <section className='section'>
-      <Wrapper className='section-center'>
+    <section className="section">
+      <Wrapper className="section-center">
         <Card></Card>
         <Followers></Followers>
       </Wrapper>
     </section>
-  )
+  );
 };
 
 const Wrapper = styled.div`
--- src/context/context.js
+++ src/context/context.js
@@ -1,22 +1,66 @@
-import React, { useState, useEffect } from 'react';
-import mockUser from './mockData.js/mockUser';
-import mockRepos from './mockData.js/mockRepos';
-import mockFollowers from './mockData.js/mockFollowers';
-import axios from 'axios';
+import React, { useState, useEffect } from "react";
+import mockUser from "./mockData.js/mockUser";
+import mockRepos from "./mockData.js/mockRepos";
+import mockFollowers from "./mockData.js/mockFollowers";
+import axios from "axios";
 
-const rootUrl = 'https://api.github.com';
+const rootUrl = "https://api.github.com";
 
 const GithubContext = React.createContext();
 
-const GithubProvider = ({children}) => {
-   const [githubUser, setGithubUser] = useState(mockUser);
-   const [repos, setRepos] = useState(mockRepos);
-   const [followers, setFollowers] = useState(mockFollowers);
-    return (
-    <GithubContext.Provider value={{githubUser, repos, followers}}>{children}</GithubContext.Provider> //githubuser = githubuser dedi eger bilmme ne olursa sadece propeerty adini yaziyourz?
-    )
-}
+const GithubProvider = ({ children }) => {
+  const [githubUser, setGithubUser] = useState(mockUser);
+  const [repos, setRepos] = useState(mockRepos);
+  const [followers, setFollowers] = useState(mockFollowers);
+  const [requests, setRequests] = useState(0);
+  const [loading, setIsLoading] = useState(false);
+  const [error, setError] = useState({ show: false, msg: "" });
 
+  const searchGithubUser = async (user) => {
+    const response = await axios(`${rootUrl}/users/${user}`).catch((err) =>
+      console.log(err)
+    );
+    console.log(response);
+    if (response) {
+      setGithubUser(response.data);
+    } else {
+      toggleError(true, "there is no user with that username");
+    }
+  };
 
-export{GithubProvider, GithubContext}
+  const checkRequests = () => {
+    axios(`${rootUrl}/rate_limit`)
+      .then(({ data }) => {
+        let {
+          rate: { remaining },
+        } = data;
+        setRequests(remaining);
+        if (remaining === 0) {
+          toggleError(true, "sorry, you have exceeded your hourly rate limit!");
+        }
+      })
+      .catch((err) => console.log(err));
+  };
 
+  function toggleError(show = false, msg = "") {
+    setError({ show, msg });
+  }
+  useEffect(checkRequests, []);
+
+  return (
+    <GithubContext.Provider
+      value={{
+        githubUser,
+        repos,
+        followers,
+        requests,
+        error,
+        searchGithubUser,
+      }}
+    >
+      {children}
+    </GithubContext.Provider>
+  );
+};
+
+export { GithubProvider, GithubContext };
